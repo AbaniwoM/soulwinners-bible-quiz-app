32dc3f77b1e0be524b854bc95ff54eed981f7731
AbaniwoM <michaelabaniwo@gmail.com>
2021-11-25 14:55:22 +0100

    built quiz app

---
+++ src/components/Quiz.js
@@ -0,0 +1,53 @@
+import {useContext} from "react";
+import {QuizContext} from "../contexts/quiz";
+import Question from "./Question";
+import { GiDove } from "react-icons/gi";
+import { ImTwitter, ImFacebook, ImWhatsapp } from "react-icons/im";
+import { BsInstagram, BsYoutube } from "react-icons/bs";
+
+const Quiz = () => {
+    const [quizState, dispatch] = useContext(QuizContext);
+    console.log("quizState", quizState);
+    return (
+    <div className="quiz">
+        {quizState.showResults && (
+            <div className="results">
+                <div className="congratulations">Congratulations!</div>
+                <div className="results-info">
+                    <div>You have completed the quiz.</div>
+                    <div className="got">You've got {quizState.correctAnswerCount} of {quizState.questions.length} right.</div>
+                    <div className="restart-button" onClick={() => dispatch({ type: "RESTART" })}>Restart</div>
+                </div>
+            </div>
+        )}
+        {!quizState.showResults && (
+            <div className="container">
+            <div className="header">
+                <div className="dove">
+                    <GiDove />
+                </div>
+                <h1>THE SOUL WINNERS BIBLE QUIZ APP</h1>
+            </div>
+            <div className="score">
+            Question {quizState.currentQuestionIndex + 1}/{quizState.questions.length}
+            </div> 
+            <Question />
+            <div className="next-button" onClick={() => dispatch({ type: "NEXT_QUESTION" })}>Next Question</div>
+            <div className="social-icons">
+                    <div className="social"><a href="https://web.facebook.com/dsoulwinners"><ImFacebook /></a></div>
+                    <div className="social"><a href="https://www.youtube.com/channel/UCC8rsiKPmRINhEdz4FRYRkw"><BsYoutube /></a></div>
+                    <div className="social"><a href="https://www.instagram.com/thesoulwinners/"><BsInstagram /></a></div>
+                    <div className="social"><a href="https://mobile.twitter.com/_thesoulwinners"><ImTwitter /></a></div>
+                    <div className="social"><a href="https://chat.whatsapp.com/DNGOnLvPmkNG6mFfof1Y7B"><ImWhatsapp /></a></div>
+            </div>
+            <div className="copy-right">
+               <h3>Built by <a href="https://mobile.twitter.com/_codedr">@_codedr</a></h3>
+               <h4>&copy; 2021</h4>
+            </div>
+            </div>
+        )}
+    </div>
+    )
+};
+
+export default Quiz;
---
+++ src/data.js
@@ -0,0 +1,84 @@
+const data = [
+    {
+      question: "Who wrote the book of Jude?",
+      incorrectAnswers: [
+        "Paul",
+        "Jude",
+        "James",
+      ],
+      correctAnswer: "Jude the brother of Jesus Christ",
+    },
+  
+    {
+      question:
+        "Who wrote the book of James?",
+      incorrectAnswers: [
+        "Jude",
+        "Peter",
+        "John",
+      ],
+      correctAnswer: "James the brother of Jesus Christ",
+    },
+    {
+      question: "What is the new commandment according to Jesus Christ?",
+      incorrectAnswers: ["10 Commandments", "Give", "Pray"],
+      correctAnswer: "Love",
+    },
+    {
+      question: "Who wrote the book of Job?",
+      incorrectAnswers: ["Job", "David", "Solomon"],
+      correctAnswer: "Moses",
+    },
+    {
+      question: "Who wrote the book of Genesis?",
+      incorrectAnswers: [
+        "Adam",
+        "Abraham",
+        "Jacob",
+      ],
+      correctAnswer: "Moses",
+    },
+    {
+      question: "Which King was deceived to go to Battle with the king of Israel?",
+      incorrectAnswers: [
+        "Ahab",
+        "Saul",
+        "David",
+      ],
+      correctAnswer: "Jehoshaphat",
+    },
+    {
+      question: "How many times did Naaman have to dip himself in the River Jordan to be healed?",
+      incorrectAnswers: ["10", "8", "5"],
+      correctAnswer: "7",
+    },
+    {
+      question: "Which of Paul's followers abandoned the ministry and forsook him?",
+      incorrectAnswers: [
+        "Timothy",
+        "Barnabas",
+        "John",
+      ],
+      correctAnswer: "Damas",
+    },
+    {
+        question: "How many years did the Israelites spend in Egypt before God delivered them?",
+        incorrectAnswers: [
+          "400",
+          "40",
+          "500",
+        ],
+        correctAnswer: "430",
+    },
+    {
+        question: "Which two women were recorded as Heroes of Faith according to Hebrews 11?",
+        incorrectAnswers: [
+          "Deborah and Esther",
+          "Ruth and Mary",
+          "Naomi and Elizabeth",
+        ],
+        correctAnswer: "Sarah and Rahab",
+    },
+  ];
+  
+  export default data;
